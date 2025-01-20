5ce09f457364956d738b86c63bfacf324a496a0b
Hasmukh Patel <hasmukhlalpatel@live.co.uk>
2025-01-20 19:31:31 +0000

    http tesdt not working

--- test/Azure.KeyVault.Simulator.E2E.Tests/BasicTests.cs
+++ test/Azure.KeyVault.Simulator.E2E.Tests/BasicTests.cs
@@ -2,6 +2,8 @@ using Azure.Security.KeyVault.Secrets;
 using Azure.Identity;
 using Azure.Security.KeyVault.Certificates;
 using System.Security.Cryptography.X509Certificates;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace Azure.KeyVault.Simulator.E2E.Tests
 {
@@ -36,6 +38,57 @@ namespace Azure.KeyVault.Simulator.E2E.Tests
 
             var x509Certificate2 = new X509Certificate2(pfxBytes);
 
+        }
+
+        [Fact] // not working
+        public async Task TestCertificateWithHttp() 
+        {
+            string keyVaultUrl = "https://<YourKeyVaultName>.vault.azure.net/";
+            string certificateName = "<YourCertificateName>";
+            string apiVersion = "7.3"; // Key Vault API version
+
+            keyVaultUrl = "https://akv-test.vault.azure.net/";
+            certificateName = "localhostcaroot";
+
+            // Obtain an access token using DefaultAzureCredential
+            var credential = new DefaultAzureCredential();
+            var token = await credential.GetTokenAsync(
+                new Azure.Core.TokenRequestContext(new[] { "https://vault.azure.net/.default" })
+            );
+
+            // Use HttpClient to fetch the certificate secret
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Token);
+
+                // Fetch the certificate secret
+                string secretUrl = $"{keyVaultUrl}secrets/{certificateName}?api-version={apiVersion}";
+                HttpResponseMessage response = await httpClient.GetAsync(secretUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    var secretResponse = JsonSerializer.Deserialize<SecretResponse>(responseBody);
+
+                    // Decode the Base64-encoded PFX value
+                    byte[] pfxBytes = Convert.FromBase64String(secretResponse.Value);
+
+                    // Save the PFX file
+                    string filePath = $"{certificateName}.pfx";
+                    await File.WriteAllBytesAsync(filePath, pfxBytes);
+                    Console.WriteLine($"Certificate saved to: {filePath}");
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode}, {await response.Content.ReadAsStringAsync()}");
+                }
+            }
+        }
+
+        // Helper class to deserialize the secret response
+        public class SecretResponse
+        {
+            public string Value { get; set; }
         }
     }
 }
