6b48d3b4a36f64e2e4c47b6c631c33f8f5b4a1d6
Hasmukh Patel <hasmukhlalpatel@live.co.uk>
2025-01-20 19:19:17 +0000

    added test for get certificate

--- test/Azure.KeyVault.Simulator.E2E.Tests/BasicTests.cs
+++ test/Azure.KeyVault.Simulator.E2E.Tests/BasicTests.cs
@@ -1,16 +1,41 @@
 using Azure.Security.KeyVault.Secrets;
 using Azure.Identity;
+using Azure.Security.KeyVault.Certificates;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Azure.KeyVault.Simulator.E2E.Tests
 {
     public class BasicTests
     {
+        //string akvUrl = "https://akv-test.vault.azure.net/";
+        string akvUrl = "https://localhost:5006/";
+
         [Fact]
-        public async Task Test1()
+        public async Task TestSecret()
         {
-            var secretClient = new SecretClient(new Uri("https://localhost:5006/"), new DefaultAzureCredential());
+            var secretClient = new SecretClient(new Uri(akvUrl), new DefaultAzureCredential());
             var secret = await secretClient.GetSecretAsync("SampleKey");
             Assert.Equal(secret.Value.Value, "SampleValue");
+        }
+
+        //
+
+        [Fact]
+        public async Task TestCertificate()
+        {
+            var certificateClient = new CertificateClient(new Uri(akvUrl), new DefaultAzureCredential());
+            var certificateWithPolicy = await certificateClient.GetCertificateAsync("localhostcaroot");
+            Assert.NotNull(certificateWithPolicy.Value);
+            var x509Certificate1 = new X509Certificate2(certificateWithPolicy.Value.Cer);
+
+            var secretClient = new SecretClient(new Uri(akvUrl), new DefaultAzureCredential());
+            var certificateSecret = await secretClient.GetSecretAsync("localhostcaroot");
+            Assert.NotNull(certificateSecret.Value.Value);
+
+            byte[] pfxBytes = Convert.FromBase64String(certificateSecret.Value.Value);
+
+            var x509Certificate2 = new X509Certificate2(pfxBytes);
+
         }
     }
 }
